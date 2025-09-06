6119280ffd522420079e89c0043085ed4e8584b5
ariebesehl <a.riebesehl@gmail.com>
2025-09-06 17:16:33 +0200

    build: locally rsynced dev/git now copies symlink target, rather than symlink itself, into git stage

--- at328/ZDX__SHARED__DEF.h
+++ at328/ZDX__SHARED__DEF.h
@@ -1,1 +1,38 @@
-../../ZDK/ZDX/ZDX__SHARED__DEF.h
+/*** Copyright (C) 2019-2025 ZaidaTek, Andreas Riebesehl
+**** This work is licensed under: Creative Commons Attribution-NoDerivatives 4.0 International Public License
+**** For full license text, please visit: https://creativecommons.org/licenses/by-nd/4.0/legalcode
+***/
+#ifndef ZDX__SHARED__DEF_H_INCLUDED
+#define ZDX__SHARED__DEF_H_INCLUDED
+/*
+#define ZDX_MSG_PREFIX 0xff
+#define ZDX_MSG_VERSION_MAJOR 0x1
+#define ZDX_MSG_VERSION_MINOR 0x1
+#define ZDX_MSG_VERSION ((ZDX_MSG_VERSION_MAJOR << 8) | ZDX_MSG_VERSION_MINOR)
+#define ZDX_MSG_CRC_SALT 0xa5
+#define ZDX_MSG_LENGTH_HEADER 8
+
+#define ZDX_MSGX_PREFIX 0
+#define ZDX_MSGX_JOB 1
+#define ZDX_MSGX_VMAJ 2
+#define ZDX_MSGX_VMIN 3
+#define ZDX_MSGX_LENH 4
+#define ZDX_MSGX_LENL 5
+#define ZDX_MSGX_CRCPL 6
+#define ZDX_MSGX_CRCHD 7
+
+#define ZDX_JOB_END 0x0
+#define ZDX_JOB_WAIT 0x1
+#define ZDX_JOB_TASK 0x10
+#define ZDX_JOB_ERR_TIMEOUT 0x81
+#define ZDX_JOB_ERR_CRC 0x82
+#define ZDX_JOB_ERR_VERSION 0x83
+*/
+
+#define ZDX_TASK_DII 0x10
+#define ZDX_TASK_DIO 0x20
+#define ZDX_TASK_PWM 0x21
+#define ZDX_TASK_ADC 0x40
+#define ZDX_TASK_DAC 0x80
+
+#endif // ZDX__SHARED__DEF_H_INCLUDED
--- at328/ZDX__SHARED__UAT328.h
+++ at328/ZDX__SHARED__UAT328.h
@@ -1,1 +1,29 @@
-../../ZDK/ZDX/ZDX__SHARED__UAT328.h
+/*** Copyright (C) 2019-2025 ZaidaTek, Andreas Riebesehl
+**** This work is licensed under: Creative Commons Attribution-NoDerivatives 4.0 International Public License
+**** For full license text, please visit: https://creativecommons.org/licenses/by-nd/4.0/legalcode
+***/
+#ifndef ZDX__SHARED__UAT328_H_INCLUDED
+#define ZDX__SHARED__UAT328_H_INCLUDED
+// USER
+#define ZDX_AT328_BUFFER 16
+#define ZDX_AT328_TASK (ZDX_TASK_DIO | ZDX_TASK_PWM | ZDX_TASK_ADC)
+#define ZDX_AT328_DIO_MASK 0xff
+#define ZDX_AT328_PWM_MASK 0xff
+#define ZDX_AT328_PWM_RATE_MIN 100
+#define ZDX_AT328_PWM_RATE_MAX 50000
+#define ZDX_AT328_ADC_MASK 0xff
+#define ZDX_AT328_ADC_RESOLUTION 10
+#define ZDX_AT328_ADC_RATE_MIN 100
+#define ZDX_AT328_ADC_RATE_MAX 10000000
+// COMPOUND
+#define ZDX_AT328_ADC_CHANNELS (\
+(ZDX_AT328_ADC_MASK & 0x80 ? 1 : 0) + \
+(ZDX_AT328_ADC_MASK & 0x40 ? 1 : 0) + \
+(ZDX_AT328_ADC_MASK & 0x20 ? 1 : 0) + \
+(ZDX_AT328_ADC_MASK & 0x10 ? 1 : 0) + \
+(ZDX_AT328_ADC_MASK & 0x8 ? 1 : 0) + \
+(ZDX_AT328_ADC_MASK & 0x4 ? 1 : 0) + \
+(ZDX_AT328_ADC_MASK & 0x2 ? 1 : 0) + \
+(ZDX_AT328_ADC_MASK & 0x1 ? 1 : 0))
+
+#endif // ZDX__SHARED__UAT328_H_INCLUDED
