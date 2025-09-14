8a43ea167f7941e7badfb69a2fe7e40bc690f363
ariebesehl <a.riebesehl@gmail.com>
2025-09-14 02:40:11 +0200

    code/at328p: (tcal/no change) updated info notice in .ino file

--- at328/at328.ino
+++ at328/at328.ino
@@ -1,3 +1,4 @@
-/*** REQUIRES './at328.c' TO BE PRESENT
-**** FOR CONVENIENCE
-***/
+/*** This './at328.ino' requires its corresponding './at328.c' to be present
+**** It should be automatically opened by the IDE in a second tab
+**** For convenience
+***/// CODE IS IN SECOND TAB
